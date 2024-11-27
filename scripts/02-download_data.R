81bf836cd527083ee44e6dca036d76814647ecf2
danield424 <danieldu42466@gmail.com>
2024-11-27 15:10:32 -0500

    added note

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -25,4 +25,5 @@ shots2024_25 <- read_csv("data/01-raw_data/shots_2024.csv")
 # change the_raw_data to whatever name you assigned when you downloaded it.
 # write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
+todo: SAVE AS PARQUET FILE
          
